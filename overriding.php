28e64f4a7629f932a0890454dcc4882ab18d85b6
eugenezzzxxx <eugenezzzxxx@gmail.com>
2021-04-14 17:40:25 +0300

    Create overriding.php

---
+++ overriding.php
@@ -0,0 +1,27 @@
+<?php
+
+// переменная типа int
+$integerVariable = 100;
+
+var_dump($integerVariable);
+
+// кладу в переменную строку.
+$integerVariable = "I am a string now!";
+
+var_dump($integerVariable);
+
+// общепринятые имена переменных используемые для счетчика.
+$i = 0;
+$j = 0;
+$k = 0;
+
+$itsNumber100 = 100; // правильное имя переменной.
+$_itsNumber100 = 100; // правильное имя переменной.
+//$100itsNumber = 100; // неправильное имя переменной!!!
+$ABCD100000000 = 100; // правильно, но имя переменной не понятно!
+
+//lower camel case
+$iAmALongNamedVariable = "something";
+
+//snake case
+$i_am_a_long_named_variable = "something";
